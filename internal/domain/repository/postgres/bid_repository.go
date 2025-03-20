cba042f32ba9dd94c0f84d5cc75b999d2471c72d
Kian <kianyarii1383@gmail.com>
2025-03-20 15:46:49 +0330

    ops: deploy corp

--- internal/domain/repository/postgres/bid_repository.go
+++ internal/domain/repository/postgres/bid_repository.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"github.com/BargheNo/Backend/internal/domain/entity"
+	"github.com/BargheNo/Backend/internal/domain/enums"
 	"github.com/BargheNo/Backend/internal/infrastructure/database"
 )
 
 type BidRepository interface {
-	GetInstallationRequests(db database.Database, status string, corporationID uint, offset int, pageSize int, sortBy string, dir string) []*entity.InstallationRequest
-	GetRandomOpenInstallationRequests(db database.Database, corporationID uint, pageSize int, offset int) []*entity.InstallationRequest
+	GetInstallationRequests(db database.Database, status enums.InstallationRequestStatus, corporationID uint, offset int, pageSize int, sortBy string, dir string) []*entity.InstallationRequest
+	GetRandomInstallationRequests(db database.Database, status enums.InstallationRequestStatus, corporationID uint, pageSize int, offset int) []*entity.InstallationRequest
 	FindInstallationRequestByID(db database.Database, id uint) (*entity.InstallationRequest, bool)
 	CreateBid(db database.Database, bid *entity.Bid) error
 	FindBidByID(db database.Database, id uint) (*entity.Bid, bool)
