076f32198a5b1e4bcd40fe46da39935488a7b038
Kian <kianyarii1383@gmail.com>
2025-05-02 05:16:55 +0330

    test: Does User Exist

--- internal/application/service/user_service_test.go
+++ internal/application/service/user_service_test.go
@@ -1,12 +1,17 @@
 package serviceimpl_test
 
 import (
+	"testing"
+
 	"github.com/BargheNo/Backend/bootstrap"
 	serviceimpl "github.com/BargheNo/Backend/internal/application/service"
+	"github.com/BargheNo/Backend/internal/domain/entity"
 	"github.com/BargheNo/Backend/mocks"
+	"github.com/stretchr/testify/suite"
 )
 
 type UserServiceTestSuite struct {
+	suite.Suite
 	constants           *bootstrap.Constants
 	otpService          *mocks.OtpServiceMock
 	jwtService          *mocks.JwtServiceMock
@@ -43,4 +48,32 @@ func (s *UserServiceTestSuite) SetupTest() {
 		DB:                  s.db,
 	}
 	s.userService = serviceimpl.NewUserService(deps)
+}
+
+func (s *UserServiceTestSuite) TestDoesUserExist() {
+	s.Run("success - User exists", func() {
+		userID := uint(1)
+		s.userRepository.On("FindUserByID", s.db, userID).Return(&entity.User{}, true).Once()
+
+		s.userService.DoesUserExist(userID)
+
+		s.userRepository.AssertExpectations(s.T())
+	})
+
+	s.Run("Error - User does not exist", func() {
+		userID := uint(1)
+		var nilUser *entity.User = nil
+
+		s.userRepository.On("FindUserByID", s.db, userID).Return(nilUser, false).Once()
+
+		s.Panics(func() {
+			s.userService.DoesUserExist(userID)
+		})
+
+		s.userRepository.AssertExpectations(s.T())
+	})
+}
+
+func TestUserService(t *testing.T) {
+	suite.Run(t, new(UserServiceTestSuite))
 }
