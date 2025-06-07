3cfba0b0e4913a83ea35bbe0305eb020f4243668
Kian <kianyarii1383@gmail.com>
2025-06-08 02:12:18 +0330

    refactor: maintenance svc and repo

--- internal/domain/repository/postgres/maintenance_repository.go
+++ internal/domain/repository/postgres/maintenance_repository.go
@@ -9,13 +9,13 @@ import (
 type MaintenanceRepository interface {
 	CreateMaintenanceRecord(db database.Database, maintenanceRecord *entity.MaintenanceRecord) error
 	CreateMaintenanceRequest(db database.Database, maintenanceRequest *entity.MaintenanceRequest) error
-	FindCorporationRequestByStatus(db database.Database, requestID, corporationID uint, allowedStatus []enum.MaintenanceRequestStatus) (*entity.MaintenanceRequest, bool)
-	FindCorporationRequestsByStatus(db database.Database, corporationID uint, allowedStatuses []enum.MaintenanceRequestStatus, opts ...QueryModifier) []*entity.MaintenanceRequest
-	FindRecordByID(db database.Database, recordID uint) (*entity.MaintenanceRecord, bool)
-	FindRecordByRequestID(db database.Database, requestID uint) (*entity.MaintenanceRecord, bool)
-	FindRequestByID(db database.Database, requestID uint) (*entity.MaintenanceRequest, bool)
-	FindRequestsByCustomerID(db database.Database, customerID uint, allowedStatus []enum.MaintenanceRequestStatus, opts ...QueryModifier) []*entity.MaintenanceRequest
-	FindRequestsByPanelIDAndStatus(db database.Database, panelID uint, allowedStatus []enum.MaintenanceRequestStatus, opts ...QueryModifier) []*entity.MaintenanceRequest
+	FindCorporationRequestByStatus(db database.Database, requestID, corporationID uint, allowedStatus []enum.MaintenanceRequestStatus) (*entity.MaintenanceRequest, error)
+	FindCorporationRequestsByStatus(db database.Database, corporationID uint, allowedStatuses []enum.MaintenanceRequestStatus, opts ...QueryModifier) ([]*entity.MaintenanceRequest, error)
+	FindRecordByID(db database.Database, recordID uint) (*entity.MaintenanceRecord, error)
+	FindRecordByRequestID(db database.Database, requestID uint) (*entity.MaintenanceRecord, error)
+	FindRequestByID(db database.Database, requestID uint) (*entity.MaintenanceRequest, error)
+	FindRequestsByCustomerID(db database.Database, customerID uint, allowedStatus []enum.MaintenanceRequestStatus, opts ...QueryModifier) ([]*entity.MaintenanceRequest, error)
+	FindRequestsByPanelIDAndStatus(db database.Database, panelID uint, allowedStatus []enum.MaintenanceRequestStatus, opts ...QueryModifier) ([]*entity.MaintenanceRequest, error)
 	UpdateMaintenanceRecord(db database.Database, maintenanceRecord *entity.MaintenanceRecord) error
 	UpdateMaintenanceRequest(db database.Database, maintenanceRequest *entity.MaintenanceRequest) error
 }
