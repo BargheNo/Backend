3daa57f7622028abeb116300a2452009a8fca833
Kian <kianyarii1383@gmail.com>
2025-06-08 02:17:53 +0330

    refactor: guarantee svc and repo

--- internal/domain/repository/postgres/guarantee_repository.go
+++ internal/domain/repository/postgres/guarantee_repository.go
@@ -7,15 +7,15 @@ import (
 )
 
 type GuaranteeRepository interface {
-	FindGuaranteeByID(db database.Database, guaranteeID uint) (*entity.Guarantee, bool)
-	FindCorporationGuarantee(db database.Database, guaranteeID, corporationID uint) (*entity.Guarantee, bool)
-	FindCorporationGuaranteeByName(db database.Database, corporationID uint, name string) (*entity.Guarantee, bool)
-	FindCorporationGuarantees(db database.Database, corporationID uint, allowedStatus []enum.GuaranteeStatus) []*entity.Guarantee
-	FindGuaranteeTerms(db database.Database, guaranteeID uint) []*entity.GuaranteeTerm
+	FindGuaranteeByID(db database.Database, guaranteeID uint) (*entity.Guarantee, error)
+	FindCorporationGuarantee(db database.Database, guaranteeID, corporationID uint) (*entity.Guarantee, error)
+	FindCorporationGuaranteeByName(db database.Database, corporationID uint, name string) (*entity.Guarantee, error)
+	FindCorporationGuarantees(db database.Database, corporationID uint, allowedStatus []enum.GuaranteeStatus) ([]*entity.Guarantee, error)
+	FindGuaranteeTerms(db database.Database, guaranteeID uint) ([]*entity.GuaranteeTerm, error)
 	CreateGuarantee(db database.Database, guarantee *entity.Guarantee) error
 	CreateGuaranteeTerms(db database.Database, terms *entity.GuaranteeTerm) error
 	UpdateGuarantee(db database.Database, guarantee *entity.Guarantee) error
-	FindPanelGuaranteeViolation(db database.Database, panelID uint) (*entity.GuaranteeViolation, bool)
+	FindPanelGuaranteeViolation(db database.Database, panelID uint) (*entity.GuaranteeViolation, error)
 	CreateGuaranteeViolation(db database.Database, violation *entity.GuaranteeViolation) error
 	UpdateGuaranteeViolation(db database.Database, violation *entity.GuaranteeViolation) error
 	DeleteGuaranteeViolation(db database.Database, violation *entity.GuaranteeViolation) error
