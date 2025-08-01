632790a83cf93884140894ddc949417c16c4a39b
daryoush-safe <118970822+daryoush-safe@users.noreply.github.com>
2025-08-01 12:31:07 +0330

    fix: fix query status while getting panels (#109)

--- internal/infrastructure/repository/postgres/installation_repository.go
+++ internal/infrastructure/repository/postgres/installation_repository.go
@@ -144,7 +144,7 @@ func (repo *InstallationRepository) FindPanelsByStatus(db database.Database, all
 
 func (repo *InstallationRepository) FindCustomerPanels(db database.Database, customerID uint, allowedStatus []enum.PanelStatus, opts ...repository.QueryModifier) ([]*entity.Panel, error) {
 	var panels []*entity.Panel
-	query := db.GetDB().Where("customer_id = ? AND status = ?", customerID, allowedStatus)
+	query := db.GetDB().Where("customer_id = ? AND status IN ?", customerID, allowedStatus)
 	for _, opt := range opts {
 		query = opt.Apply(query).(*gorm.DB)
 	}
